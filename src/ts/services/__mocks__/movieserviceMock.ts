46766c41a0ba0b10882d9781406a5e9efb9c216c
Sandra S <zandrastr@hotmail.com>
2023-02-08 16:53:11 +0100

    :clown_face: Add mock data

---
+++ src/ts/services/__mocks__/movieserviceMock.ts
@@ -0,0 +1,17 @@
+import { IMovie } from "../../models/Movie";
+
+/*
+Title: string;
+imdbID: string;
+Type: string;
+Poster: string;
+Year: string;
+*/
+
+export let mockData: IMovie[] = [{Title: "Avatar 2", imdbID: "12345", Type: "Science Fiction", Poster: "Test", Year: "2022" }];
+
+export const getData = async (searchText: string): Promise<IMovie[]> => {
+    return new Promise((resolve) => {
+        resolve(mockData);
+    });
+};
